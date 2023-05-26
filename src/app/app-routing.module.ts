da57a4b171b2fd1fb0bd8a1109fc525268a752f2
TanDung <nguyentandung9111@gmail.com>
2023-05-27 04:32:40 +0700

    init project tailwind, less

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CounterChauTranComponent } from './pages/counter-chau-tran/counter-chau-tran.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'counter-chau-tran', component: CounterChauTranComponent },
+  // { path: 'nztable', component: NzTabelComponent },
+  // { path: 'td', component: TdComponent },
+  // { path: 'th', component: ThComponent },
+  // { path: 'tr', component: TrComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/counter-chau-tran/counter-chau-tran.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter-chau-tran',
+  templateUrl: './counter-chau-tran.component.html',
+  styleUrls: ['./counter-chau-tran.component.less']
+})
+export class CounterChauTranComponent {
+
+}
